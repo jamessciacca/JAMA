00693d3f7b8498c695f1cafb8045e3e6f16acf79
James Sciacca <JamesSciacca1@mac.lan>
2026-04-09 16:22:35 -0400

    finished web application

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 
 async function parseJson(response) {
   const data = await response.json().catch(() => ({}));
